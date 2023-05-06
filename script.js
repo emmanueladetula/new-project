9bec70e57f88ce4ff98424d008b0ffe71ed3cfff
emmanueladetula <emmanueladetula002@gmail.com>
2023-05-06 08:11:28 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// const toggleBtn = document.querySelector('#toggle-btn');
+// const body = document.body;
+
+// toggleBtn.addEventListener('click', () => {
+//   body.classList.toggle('night-mode');
+// });
+
+const mod = document.querySelector('#light')
+const bod = document.querySelector('body')
+
+mod.addEventListener('click',()=>{
+
+// document.querySelector('a').innerHTML='<i class="fa fa-sun"></i>';
+document.querySelector('body').classList.toggle('dark')
+
+})
+
+
